c418319df736210c2804b5667bdf2d4b0616b113
IchNewBee <131459887+IchNewBee@users.noreply.github.com>
2024-02-01 23:13:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,67 +1,43 @@
-//Create web sever
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-var Comment = mongoose.model('Comment');
-var Post = mongoose.model('Post');
+// Create web server
+// Create HTTP server
+// Create a web server object
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+var formidable = require('formidable');
+var util = require('util');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('comments.db');
 
-//Create a comment
-router.post('/posts/:post/comments', function (req, res, next) {
-    var comment = new Comment(req.body);
-    comment.post = req.post;
-    comment.save(function (err, comment) {
-        if (err) {
-            return next(err);
-        }
-        req.post.comments.push(comment);
-        req.post.save(function (err, post) {
-            if (err) {
-                return next(err);
-            }
-            res.json(comment);
-        });
-    });
-});
-
-//Upvote a comment
-router.put('/posts/:post/comments/:comment/upvote', function (req, res, next) {
-    req.comment.upvote(function (err, comment) {
-        if (err) {
-            return next(err);
-        }
-        res.json(comment);
-    });
-});
-
-//Preload comment objects on routes with ':comment'
-router.param('comment', function (req, res, next, id) {
-    var query = Comment.findById(id);
-    query.exec(function (err, comment) {
-        if (err) {
-            return next(err);
-        }
-        if (!comment) {
-            return next(new Error('can\'t find comment'));
-        }
-        req.comment = comment;
-        return next();
-    });
-});
-
-//Preload post objects on routes with ':post'
-router.param('post', function (req, res, next, id) {
-    var query = Post.findById(id);
-    query.exec(function (err, post) {
-        if (err) {
-            return next(err);
-        }
-        if (!post) {
-            return next(new Error('can\'t find post'));
-        }
-        req.post = post;
-        return next();
-    });
-});
-
-//Export router
-module.exports = router;
+// Create a server object
+http.createServer(function (req, res) {
+  // Parse the request containing file name
+  var pathname = url.parse(req.url).pathname;
+  // Print the name of the file for which request is made
+  console.log("Request for " + pathname + " received.");
+  // Read the requested file content from file system
+  if (pathname == "/") {
+    pathname = "/index.html";
+  }
+  var filename = path.join(process.cwd(), pathname);
+  fs.readFile(filename, function(err, data){
+    if(err){
+      console.log(err);
+      // HTTP Status: 404 : NOT FOUND
+      // Content Type: text/plain
+      res.writeHead(404, {'Content-Type': 'text/html'});
+    }else{
+      //Page found
+      // HTTP Status: 200 : OK
+      // Content Type: text/plain
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      // Write the content of the file to response body
+      res.write(data.toString());
+    }
+    // Send the response body
+    res.end();
+  });
+}).listen(8081);
+// Console will print the message
+console.log('Server running at http://');
