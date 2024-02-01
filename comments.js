8621ee6fb144342def5521642d09a207f8f7ef86
IchNewBee <131459887+IchNewBee@users.noreply.github.com>
2024-02-01 23:06:06 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,67 @@
+//Create web sever
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var Comment = mongoose.model('Comment');
+var Post = mongoose.model('Post');
+
+//Create a comment
+router.post('/posts/:post/comments', function (req, res, next) {
+    var comment = new Comment(req.body);
+    comment.post = req.post;
+    comment.save(function (err, comment) {
+        if (err) {
+            return next(err);
+        }
+        req.post.comments.push(comment);
+        req.post.save(function (err, post) {
+            if (err) {
+                return next(err);
+            }
+            res.json(comment);
+        });
+    });
+});
+
+//Upvote a comment
+router.put('/posts/:post/comments/:comment/upvote', function (req, res, next) {
+    req.comment.upvote(function (err, comment) {
+        if (err) {
+            return next(err);
+        }
+        res.json(comment);
+    });
+});
+
+//Preload comment objects on routes with ':comment'
+router.param('comment', function (req, res, next, id) {
+    var query = Comment.findById(id);
+    query.exec(function (err, comment) {
+        if (err) {
+            return next(err);
+        }
+        if (!comment) {
+            return next(new Error('can\'t find comment'));
+        }
+        req.comment = comment;
+        return next();
+    });
+});
+
+//Preload post objects on routes with ':post'
+router.param('post', function (req, res, next, id) {
+    var query = Post.findById(id);
+    query.exec(function (err, post) {
+        if (err) {
+            return next(err);
+        }
+        if (!post) {
+            return next(new Error('can\'t find post'));
+        }
+        req.post = post;
+        return next();
+    });
+});
+
+//Export router
+module.exports = router;
